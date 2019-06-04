d4ca0d5be2301be692f8b517b7dc53ad84219e3b
Gaganpandher52 <gaganpandher52@gmail.com>
2019-06-04 20:28:35 +0000

    added the ChatBar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React, {Component} from 'react';
-
+import ChatBar from './ChatBar.jsx';
 class App extends Component {
+  constructor(props) {
+    super(props);
+    // this is the *only* time you should assign directly to state:
+    this.state = {loading: true};
+  }
   render() {
     return (
-      <h1></h1>
+      <div>
+        <ChatBar/>
+      </div>
+      
     );
   }
 }
 export default App;
+
