f1b541018adb12ff1b389ff57c639b6cfe1497d4
Gaganpandher52 <gaganpandher52@gmail.com>
2019-06-07 16:41:39 +0000

    fix the isssue with the counter and clean up code

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,26 @@
+//importing react and other components
 import React, {Component} from 'react';
 import ChatBar from './ChatBar.jsx';
 import MessageList from './MessageList.jsx';
 
+// parent contonent login
 class App extends Component {
-  
   constructor(props) {
     super(props);
-    // this is the *only* time you should assign directly to state:
+    //setting the state
     this.state = {loading: true,
-      currentUser : 'Anynomouse', // optional. if currentUser is not defined, it means the user is Anonymous
+      currentUser : 'Anyonnomous',
       messages: [
-       
       ]}; 
-      
   }
   onSubmit = evt => {
-    
-      if(evt.key =='Enter'){
-        const messageI = document.getElementById('chatbar-msg')
-        const msg = messageI.value;
-      
-      // const newM = evt.target.elements.currentUser;
-      // const user = evt.target.elements.user;
+    //initializa the event on enter
+    if(evt.key =='Enter'){
+      const messageI = document.getElementById('chatbar-msg')
+      const msg = messageI.value;
       const oldMessages = this.state.messages;
       const username1 = this.state.currentUser;
-      //const nameChange = evt.target.elemtent.user//new
-      
+    
       const newMess = {
         type:'postMessage',
         username: username1,
@@ -35,64 +30,55 @@ class App extends Component {
             ...oldMessages,
             newMess
           ];
-     
-      
       messageI.value = "";
       this.socket.send(JSON.stringify(newMess));
-      // this.socket.send(JSON.stringify(newChange));
-        }
+    }//if
     };
 
- onChange = evt =>{
-
+  onChange = evt =>{
     if(evt.key =='Enter'){
-    // const userq = evt.target.elements.user;
-    const nameInput = document.getElementById('chatbar-user');
-	  const newUserName = nameInput.value;
-    const newMessage = (this.state.currentUser + " changed the username to " + newUserName);
-    const newChange = {
-      type: 'postNotification',
-      content:newMessage
-    }
-  this.socket.send(JSON.stringify(newChange));
-  this.setState({ currentUser: newUserName });
+      const nameInput = document.getElementById('chatbar-user');
+      const newUserName = nameInput.value;
+      const newMessage = (this.state.currentUser + " changed the username to " + newUserName);
+      const newChange = {
+        type: 'postNotification',
+        content:newMessage
+      }
+      this.socket.send(JSON.stringify(newChange));
+      this.setState({ currentUser: newUserName });
+    }//if
   }
-
-}
 
   componentDidMount() {
-  console.log("componentDidMount <App />");
-  
-  this.socket = new WebSocket("ws://localhost:3001/")
-  
-  this.socket.onopen = () => {
-    // on connecting, do nothing but log it to the console
-    console.log('connected to the server')
-    
-  }
-  this.socket.onmessage = (event) =>{
-    console.log(event.data)
-    const obj = JSON.parse(event.data);
-    if(obj.type ==='incomingMessage'){
-    
-    const newMessage ={
-      id:obj.id,
-      username:obj.username,
-      content:obj.content
-    }
-    this.setState({messages:[...this.state.messages, newMessage]})
-    return 
-    // return
-  }
-    
-    if(obj.type ==='counter'){
-      const obj = JSON.parse(event.data);
-   
-      this.setState({counter:obj.size})
-      return
-    }
+    console.log("componentDidMount <App />");
+    this.socket = new WebSocket("ws://localhost:3001/")
+
+    this.socket.onopen = () => {
+      // on connecting, do nothing but log it to the console
+      console.log('connected to the server')
     }
 
+    this.socket.onmessage = (event) =>{
+      console.log(event.data)
+      const obj = JSON.parse(event.data);
+      //checks for message or incoming/notification
+      if(obj.type ==='incomingMessage'){
+        const newMessage ={
+          id:obj.id,
+          username:obj.username,
+          content:obj.content
+        }
+        this.setState({messages:[...this.state.messages, newMessage]})
+        return 
+      }//if
+      
+      if(obj.type ==='counter'){
+        const obj = JSON.parse(event.data);
+    
+        this.setState({counter:obj.size})
+        return
+      }//if
+    }//onmessage
   }//component
   
   render() {
@@ -109,6 +95,6 @@ class App extends Component {
       
     );
   }
-}
+}//App
 export default App;
 
